cf3de83a8d178d2338593eec057ddd07c3e92b7e
Thiago Bristot <altf4nfunfa@gmail.com>
2024-10-06 11:35:44 -0300

    Update channels.js

--- api/channels.js
+++ api/channels.js
@@ -1,12 +1,15 @@
-// api/channels.js
-
 const axios = require('axios');
 
 export default async function handler(req, res) {
+  console.log("API function called");  // Adicionando log para verificar a chamada
+
   try {
     const response = await axios.get('https://iptv-org.github.io/api/channels.json');
     const channels = response.data;
-    
+
+    // Log para ver a resposta dos canais
+    console.log('Channels fetched:', channels);
+
     res.status(200).json(channels);
   } catch (error) {
     console.error('Error fetching channels:', error);
